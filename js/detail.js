13d045866ac91e57c65293256a30e24062dfe827
国 王 <www.13083275770@outlook.com>
2020-08-15 14:04:40 +0800

    详情页

--- js/detail.js
+++ js/detail.js
@@ -8,4 +8,42 @@ window.onload = function() {
     new Swiper('.swiper-container')
     var mySwiper = document.querySelector('.swiper-container').swiper
     mySwiper.slideNext();
+    var currentDate, currentTime, next;
+    var day = document.getElementById("day");
+    var hour = document.getElementById("hour");
+    var minute = document.getElementById("minute");
+    var second = document.getElementById("second");
+    var nexttime = new Date("2020/9/11 12:00");
+    setInterval(function() {
+        currentDate = new Date();
+        currentTime = currentDate.getTime();
+        next = nexttime.getTime();
+        allTime = next - currentTime;
+        //转化
+        secondes = parseInt(allTime / 1000);
+        var d = parseInt(secondes / 3600 / 24);
+        var h = parseInt(secondes / 3600 % 24);
+        var m = parseInt(secondes / 60 % 60);
+        var s = parseInt(secondes % 60);
+        day.innerText = addzero(d);
+        hour.innerText = addzero(h);
+        minute.innerText = addzero(m);
+        second.innerText = addzero(s);
+    }, 1000);
+    // var touxiang = document.getElementById("touxiang");
+    // var txhtml = "";
+    // var list = ["/img/tx1.jpg", "/img/tx2.jpg"];
+    // for (var i = 0; i < list.length; i++) {
+    //     txhtml += '<img src=' + list[i] + ' style="z-index: ' + (list.length - i) + '; transform:translateX(' + i * 10 + 'px)">';
+    // }
+    // alert(txhtml);
+    // touxiang.innerHTML = txhtml;
+}
+
+function addzero(time) {
+    if (time > 9) {
+        return time;
+    } else {
+        return "0" + time;
+    }
 }
